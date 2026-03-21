8593fb32c2a374dcd0468842782b18ac4dd2f84a
Elijah Phillips <elp183@pitt.edu>
2026-03-21 19:56:26 -0400

    tokenizeInputString

--- project3/XMLParser.cpp
+++ project3/XMLParser.cpp
@@ -8,8 +8,95 @@ XMLParser::XMLParser()
 
 bool XMLParser::tokenizeInputString(const std::string &inputString)
 {
-	// TODO
-	return false;
+	//make sure that the stack and bag is clear
+    clear();
+    
+    int i = 0;
+    int length = inputString.length();
+    
+    while (i < length) {
+       //look for the < so show the start
+        if (inputString[i] == '<') {
+            int j = i + 1;
+            
+            //look for the > for the end
+            while (j < length && inputString[j] != '>' && inputString[j] != '<') {
+                j++;
+            }
+			//check if we run into a length issue for the < for a new line than throw false
+            if (j == length || inputString[j] == '<') {
+				//make sure next time works
+                clear(); 
+                return false; 
+            }
+            
+           //get everything between the <>
+            std::string tagContent = inputString.substr(i + 1, j - i - 1);
+            TokenStruct t;
+            
+           //check what type of message it is 
+            if (tagContent.length() >= 2 && tagContent.front() == '?' && tagContent.back() == '?') {
+                //? mean deleraration
+                t.tokenType = DECLARATION;
+                //get tid of the ? to see the real string
+                t.tokenString = tagContent.substr(1, tagContent.length() - 2);
+            } 
+            else if (!tagContent.empty() && tagContent.front() == '/') {
+                //"/" is a end_tag
+                t.tokenType = END_TAG;
+              
+                t.tokenString = tagContent.substr(1);
+            } 
+            else if (!tagContent.empty() && tagContent.back() == '/') {
+                //empty_tag
+                t.tokenType = EMPTY_TAG;
+                // Strip the '/' from the back
+                t.tokenString = tagContent.substr(0, tagContent.length() - 1);
+            } 
+            else {
+                //start_tag
+                t.tokenType = START_TAG;
+                t.tokenString = tagContent;
+            }
+
+            tokenizedInputVector.push_back(t);
+			//go to the next part after the > 
+            i = j + 1;
+			
+        } 
+        else {
+            //go until the next tag
+            int j = i;
+           
+            while (j < length && inputString[j] != '<') {
+                j++;
+            }
+            
+            std::string content = inputString.substr(i, j - i);
+            
+            //check for pure space
+            bool allSpace = true;
+            for (char conentChar : content) {
+                if (!isspace(conentChar)) {
+                    allSpace = false;
+                    break;
+                }
+            }
+            
+            //make sure the text becomes a content token
+            if (!allSpace) {
+                TokenStruct token;
+                token.tokenType = CONTENT;
+                token.tokenString = content;
+                tokenizedInputVector.push_back(token);
+            }
+            //move to the end
+            i = j; 
+        }
+    }
+    //update the flags
+    isToken = true; 
+    return true;
 } 
 
 bool XMLParser::parseTokenizedInput()
