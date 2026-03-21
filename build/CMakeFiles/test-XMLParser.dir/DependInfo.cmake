20e1fd377e34f84498eca239681910b34d3108cc
Elijah Phillips <elp183@pitt.edu>
2026-03-21 18:57:50 -0400

    STARTING REP

---
+++ build/CMakeFiles/test-XMLParser.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/elilp/Downloads/project3/project3/XMLParser.cpp" "CMakeFiles/test-XMLParser.dir/XMLParser.cpp.obj" "gcc" "CMakeFiles/test-XMLParser.dir/XMLParser.cpp.obj.d"
+  "C:/Users/elilp/Downloads/project3/project3/test_XMLParser.cpp" "CMakeFiles/test-XMLParser.dir/test_XMLParser.cpp.obj" "gcc" "CMakeFiles/test-XMLParser.dir/test_XMLParser.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/elilp/Downloads/project3/project3
+# Build directory: C:/Users/elilp/Downloads/project3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-XMLParser "test-XMLParser")
+set_tests_properties(test-XMLParser PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/elilp/Downloads/project3/project3/CMakeLists.txt;20;add_test;C:/Users/elilp/Downloads/project3/project3/CMakeLists.txt;0;")
